da5ae03a582fb0f989e66bd1fc5a5a460bea7352
ByMaluf <brenno.maluf@gmail.com>
2022-11-27 15:20:02 -0300

    Segundo Commit

--- USING4_C_SHARP/Program.cs
+++ USING4_C_SHARP/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {          
             //Nesse caso temos a variável caminho do tipo String que recebe a string do arquivo1
             string caminho = @"c:\temp\arquivo1.txt";
 
@@ -13,24 +13,25 @@
 
             try
             {
-                //Using fecha o objeto arquivo depois de seu uso
-                using (FileStream arquivo = new FileStream(caminho, FileMode.Open))
+                using (StreamReader leitor = File.OpenText(caminho)) //Abre um arquivo de texto com codificação UTF-8 existente para leitura.
+                // No caso a strig do arquivo 1 carregada pelo string caminho
                 {
-                    //Using fecha o objeto leitor depois do seu uso
-                    using (StreamReader leitor = new StreamReader(arquivo))
+                    while (!leitor.EndOfStream)//Obtém um valor que indica se a posição do fluxo atual está no final do fluxo.
+                    //Enquanto a o leitor na posição do fluxo atual Não(!) estiver no final executa a estrutura de repetição abaixo
                     {
-                        string linhas = leitor.ReadToEnd(); // Lê todos os caracteres da posição atual até o final do fluxo.
-                        Console.WriteLine(linhas); //Impressão Linhas
+                        string linhas = leitor.ReadLine();
+                        Console.WriteLine(linhas);
                     }
                 }
             }
-            catch (IOException e) // IOException - A exceção que é gerada quando ocorre um erro de E/S.
+            catch (IOException e)// IOException - A exceção que é gerada quando ocorre um erro de E/S.
             //é a classe base para exceções geradas ao acessar informações usando fluxos, arquivos e diretórios.
             {
                 Console.WriteLine("Ocorreu um erro!");
                 Console.WriteLine(e.Message);
                 //Concede acesso às propriedades necessárias para definir uma mensagem de Enfileiramento de Mensagens.
             }
+
         }
     }
 }
